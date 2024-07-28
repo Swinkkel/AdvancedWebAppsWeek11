22a15f3bfab1937b1acacf87f65eef5df6dad133
Tomi Helin <tomi.helin@student.lut.fi>
2024-07-28 12:58:47 +0300

    Task 2

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Header from './Header';
 
-function About() {
-    return (
-        <div>
-            <h1>My About</h1>
-        </div>
-    );
-}
+const About = () => {
+  const [items, setSomeStuff] = useState([]);
+
+  useEffect(() => {
+    let mounted = true;
+
+    async function fetchData () {
+      const a = await fetch('https://jsonplaceholder.typicode.com/todos');
+      const b = await a.json();
+      if (mounted) {
+        setSomeStuff(b);
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      mounted = false;
+    };
+  }, []);
+
+  return (
+    <div>
+      <Header />
+      <h2>About Page</h2>
+      <ul>
+        {items.map(item => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default About;
